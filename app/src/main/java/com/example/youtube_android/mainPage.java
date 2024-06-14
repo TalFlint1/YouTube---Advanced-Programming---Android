70a4bb2e55d91096db951f98f278d6ad72caabe4
idozur3 <ido.zur@biu.live.ac.il>
2024-06-14 17:35:45 +0300

    toolbar and mainpage

---
+++ app/src/main/java/com/example/youtube_android/mainPage.java
@@ -0,0 +1,37 @@
+package com.example.youtube_android;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.youtube_android.R;
+
+public class mainPage extends AppCompatActivity {
+    RecyclerView videoList;
+    VideoAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_page);
+
+        videoList = findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new VideoAdapter();
+        videoList.setAdapter(adapter);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
