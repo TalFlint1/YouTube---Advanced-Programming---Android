0cebfcaadb91925a11dcd6a77a8c3ed3b419ec6e
TalFlint1 <tal.flint@live.biu.ac.il>
2024-06-14 12:08:27 +0300

    save changes..

--- app/src/main/java/com/example/youtubeandroid/MainActivity.java
+++ app/src/main/java/com/example/youtubeandroid/MainActivity.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.youtube_android.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button registerButton;
     private Button loginButton;
 
